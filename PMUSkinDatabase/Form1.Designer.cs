16a92fd070eee1b0110a757f9b22ec51a1ec69e7
Astromojang <thegreatplanetmojang@gmail.com>
2016-06-18 11:07:16 -0500

    Initial commit.

---
+++ PMUSkinDatabase/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PMUSkinDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Ampharos Skin by ShadowUmbreon",
+            "Azurill Bubble Skin by pipplyRocks",
+            "Buizel Skin by Kirk",
+            "Cherubi & Cherrim Skin by pipplyRocks",
+            "Chimecho Skin by Flareon",
+            "Flareon Skin by Kirk",
+            "Aerial Avians by Agunimon",
+            "Legendary Skin by Agunimon",
+            "Raichu Skin by pipplyRocks",
+            "Umbreon Skin by princer13",
+            "Vulpix skin by Kirk",
+            "Sekia by Flare",
+            "Plusle and Minun by Flare"});
+            this.listBox1.Location = new System.Drawing.Point(134, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(212, 173);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(377, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(442, 264);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Kozuka Mincho Pro H", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(267, 420);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(418, 84);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Apply Skin!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(87, 328);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(768, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(947, 516);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PMU Skin Database";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ PMUSkinDatabase/Form1.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using NetFwTypeLib;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace PMUSkinDatabase
+{
+    public partial class Form1 : Form{
+        public Form1()
+        {
+            InitializeComponent();
+            INetFwRule firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FWRule"));
+            firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_ALLOW;
+            firewallRule.Description = "Allow downloading";
+            firewallRule.ApplicationName = "\\PMUSkinDatabase.exe";
+            firewallRule.Enabled = true;
+            firewallRule.InterfaceTypes = "All";
+            firewallRule.Name = "PMUSkinDatabase";
+
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(
+                Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            firewallPolicy.Rules.Add(firewallRule);
+        }
+
+        
+        private string stringLink;
+        private string skindirectory = "C:\\Program Files\\Pokemon Mystery Universe\\Client\\Client\\Skins";
+        private string skinfilename;
+        private string test;
+
+
+        private void pictureBox1_Click(object sender, EventArgs e){
+
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e){
+
+            if (!CheckForInternetConnection())
+            {
+                MessageBox.Show("This application needs an internet connection. Check your internet connection.", 
+                    "No Internet Connection", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if (listBox1.GetSelected(0) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/91D688473A5A4A5QQWE2025091EWQS/2lcr4og.jpg");
+            }
+
+            if (listBox1.GetSelected(1) == true){
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/9B9E06DA3A5A4A5QQWE2025092EWQS/azurill.png");
+            }
+
+            if (listBox1.GetSelected(2) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/A413ECB33A5A4A5QQWE2025099EWQS/Buizelskin2.png");
+            }
+
+            if (listBox1.GetSelected(3) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://178.33.61.6/putstorage/DownloadFileHash/A4A64A5C3A5A4A5QQWE3371594EWQS/cheribi.jpg");
+            }
+
+            if (listBox1.GetSelected(4) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/391C726B3A5A4A5QQWE2025144EWQS/chimeco.jpg");
+            }
+
+            if (listBox1.GetSelected(5) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/C9CFAB9B3A5A4A5QQWE2025109EWQS/Flareonthememainmenu.png");
+            }
+
+            if (listBox1.GetSelected(6) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/A66B1CE73A5A4A5QQWE2025118EWQS/Ariel.jpg");
+            }
+
+            if (listBox1.GetSelected(7) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://178.33.61.6/putstorage/DownloadFileHash/F80BBDB03A5A4A5QQWE3371586EWQS/Legends.jpg");
+            }
+
+            if (listBox1.GetSelected(8) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/551E37E43A5A4A5QQWE2025137EWQS/Raichu.jpg");
+            }
+
+            if (listBox1.GetSelected(9) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/8FE876173A5A4A5QQWE2025138EWQS/6095497493_dc292804a1_b.jpg");
+            }
+
+            if (listBox1.GetSelected(10) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/755CE3F83A5A4A5QQWE2025102EWQS/vulpix.png");
+            }
+
+            if (listBox1.GetSelected(11) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/738BEC983A5A4A5QQWE2025198EWQS/Sekia.png");
+            }
+
+            if (listBox1.GetSelected(12) == true)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load("http://srv70.putdrive.com/putstorage/DownloadFileHash/D0A544BA3A5A4A5QQWE2025197EWQS/minunplusapreview1.png");
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(skindirectory) == false)
+            {
+                MessageBox.Show("Select the correct folder of your Pokemon Mystery Universe skins. This is usually located in Local Disk, Program Files, Pokemon Mystery Universe, Client, Client, Skins.",
+                    "Missing Directory", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    skindirectory = folderBrowserDialog1.SelectedPath;
+                }
+            }
+            if (Directory.Exists(skindirectory + "\\Downloads") == false)
+            {
+                Directory.CreateDirectory(skindirectory + "\\Downloads");
+            }
+
+            if (listBox1.GetSelected(0) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/B12F2D953A5A4A5QQWE2025169EWQS/Ampharos%20Theme.zip";
+
+            }
+
+            if (listBox1.GetSelected(1) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/928B371C3A5A4A5QQWE2025164EWQS/Azurill.pmuskn.zip";
+
+            }
+
+            if (listBox1.GetSelected(2) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/0B6FFC463A5A4A5QQWE2025165EWQS/Buizel%20Theme.zip";
+
+            }
+
+            if (listBox1.GetSelected(3) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/28FB8BC23A5A4A5QQWE2025167EWQS/Cherubi%20_%20Cherrim.pmuskn.zip";
+
+            }
+
+            if (listBox1.GetSelected(4) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/4205DE6A3A5A4A5QQWE2025264EWQS/Chimeco.zip";
+
+            }
+
+            if (listBox1.GetSelected(5) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/A656EEDE3A5A4A5QQWE2025172EWQS/Flareon%20Theme.zip";
+
+            }
+
+            if (listBox1.GetSelected(6) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/65DFF8453A5A4A5QQWE2025171EWQS/Aerial%20Avians%20Skin.zip";
+
+            }
+
+            if (listBox1.GetSelected(7) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/0FEB62DD3A5A4A5QQWE2025168EWQS/Legendary%20Skin%20PMU.zip";
+
+            }
+
+            if (listBox1.GetSelected(8) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/C53A35753A5A4A5QQWE2025166EWQS/Raichu%20skin.pmuskn.zip";
+
+            }
+
+            if (listBox1.GetSelected(9) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/B6479BED3A5A4A5QQWE2025170EWQS/Umbreon%20skin.pmuskn.zip";
+
+            }
+
+            if (listBox1.GetSelected(10) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/150D02093A5A4A5QQWE2025263EWQS/Vulpix.zip";
+
+            }
+
+            if (listBox1.GetSelected(11) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/0A58E6283A5A4A5QQWE2025195EWQS/Seika%20with%20Music.zip";
+
+            }
+
+            if (listBox1.GetSelected(12) == true)
+            {
+                stringLink = "http://srv70.putdrive.com/putstorage/DownloadFileHash/C02B2A313A5A4A5QQWE2025246EWQS/Minun%20_%20Plusle.pmuskn.zip";
+
+            }
+
+            WebClient client = new WebClient();
+
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+
+            Uri uri = new Uri(stringLink);
+            skinfilename = System.IO.Path.GetFileName(uri.LocalPath);
+            client.DownloadFileAsync(new Uri(stringLink), Path.Combine(skindirectory, "Downloads\\tempskin.zip"));
+            test = Path.Combine(skindirectory, "Downloads");
+
+        }
+        
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MessageBox.Show("Download completed. Skin will now be applied.",
+                    "Download Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ZipFile.ExtractToDirectory(Path.Combine(skindirectory, "Downloads\\tempskin.zip"), skindirectory);
+            File.Delete(Path.Combine(skindirectory, "Downloads\\tempskin.zip"));
+
+            MessageBox.Show("Skin has been applied! Go into game, click skins, then click the words Main Theme, and choose the new one!",
+                    "Finished", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
